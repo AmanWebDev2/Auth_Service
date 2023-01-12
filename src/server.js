da979523fcd0afbf5d9b438e065b4a90e8208bb3
gaurav <amanrawat.ar321@gmail.com>
2023-01-12 21:42:08 +0530

    signup done

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 
 const setupAndStartServer = async = () =>{
     const app = express();
 
-    app.listen(3001,()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api',apiRoutes);
+
+    app.listen(PORT,async()=>{
         console.log('server is running at port',PORT);
     })
-}
+};
+
+setupAndStartServer();

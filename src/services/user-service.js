901d49db12b2f1e33db54069546ba9c4054de362
gaurav <amanrawat.ar321@gmail.com>
2023-01-12 22:37:03 +0530

    added getuserbyid

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -15,6 +15,26 @@ class UserService {
             throw { error };
         }
     }
+
+    async destroy(userId) {
+        try {
+            const response = await this.userRepository.destroy(userId);
+            return response;
+        } catch (error) {
+            console.log('something went wrong in service layer');
+            throw { error };
+        }
+    }
+
+    async getById(userId) {
+        try {
+            const user = this.userRepository.getById(userId);
+            return user;
+        } catch (error) {
+            console.log('something went wrong in service layer');
+            throw { error };
+        }
+    }
 }
 
 module.exports = UserService;

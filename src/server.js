0aab5b30866387070448df0d90a68600080ddebf
gaurav <amanrawat.ar321@gmail.com>
2023-01-14 00:40:16 +0530

    added user roles through user_roles

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+const { User,Role } = require('./models/index');
+
 const setupAndStartServer = async = () =>{
     const app = express();
 
@@ -12,8 +14,19 @@ const setupAndStartServer = async = () =>{
 
     app.use('/api',apiRoutes);
 
+    if(process.env.DB_SYNC) {
+        db.sequelize.sync({ alter: true });
+    }
+
+    
     app.listen(PORT,async()=>{
         console.log('server is running at port',PORT);
+        // const u1 = await User.findByPk(3);
+        // const r1 = await Role.findByPk(1);
+        // u1.addRoles(r1);
+        // const response =  await u1.getRoles();
+        // const response = await r1.getUsers();
+        // console.log(response);
     })
 };
 
